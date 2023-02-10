7ec212bbf603a1cf5db266bc6b875bf549e105ba
Kaining <kainingchen2013@gmail.com>
2023-02-10 12:55:05 -0500

    problem_016_done

--- problems/problem_016.py
+++ problems/problem_016.py
@@ -3,4 +3,7 @@
 # make sure they're between 0 and 10, inclusive.
 
 def is_inside_bounds(x, y):
-    pass
+    if x in range(1,11) and y in range(1,11):
+        return True
+    else:
+        return False
