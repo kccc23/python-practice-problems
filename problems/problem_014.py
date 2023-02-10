7c5945b79b6fa7c415e21de384b6223826fd3c8e
Kaining <kainingchen2013@gmail.com>
2023-02-10 12:54:20 -0500

    problem_014_done

--- problems/problem_014.py
+++ problems/problem_014.py
@@ -11,4 +11,8 @@
 # problem to get a good feel for how to solve it.
 
 def can_make_pasta(ingredients):
-    pass
+    if "flour" in ingredients and "eggs" in ingredients and "oil" in ingredients:
+        return True
+    else:
+        return False
+
